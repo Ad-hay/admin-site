ff0500d9c2538c0be20fe021fc34cd5f22df3e5d
Ad-hay <99274118+Ad-hay@users.noreply.github.com>
2022-05-26 20:35:51 +0100

    Initial commit

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856213',
+        paymentStatus: 'due',
+        shipping: 'pending'
+    }
+]
